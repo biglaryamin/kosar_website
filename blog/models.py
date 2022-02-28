d6db1f270bbf947fe31ecc3deb5a03afff9dcaab
amin <biglarymohamadamin75@gmail.com>
2022-02-28 13:45:58 +0330

    account models added

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class Article(models.Model):
-    Title=models.CharField(max_length=100)
-    Description=models.TextField()
-    # Image=models.ImageField()
-
-    def __str__(self):
-        return self.Title
