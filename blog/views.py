e2974e261c85965a591c3e7c56fc1757ca78f3b3
amin <biglarymohamadamin75@gmail.com>
2022-04-17 23:08:47 +0430

    category_view added. In blog app, I add a Class Based View(ListView) in view.py ==> CategoryList

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
 from .models import Article,Category
@@ -17,16 +18,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import ArticleSerializer
 
-'''
-def home(request, page=1):
-	articles_list=Article.objects.published()
-	paginator    = Paginator(articles_list, 3) # Show 3 contacts per
-	articles     = paginator.get_page(page)
-	context={
-		"articles":articles,
-	}
-	return render(request,"blog/home.html",context)
-	'''
+
 
 class ArticleList(ListView):
 #	model=Article
@@ -38,22 +30,9 @@ class ArticleList(ListView):
 	def get_context_data(self,**kwargs):
 		context = super(ArticleList,self).get_context_data(**kwargs)
 		context['last_three_articles'] = Article.objects.published().order_by('-publish')[0:3]
-		# context['all_comments'] = Article.comments.all().count()
+		context['cats'] = Category.objects.all()
 		return context
 
-
-
-'''
-def detail(request,slug):
-	context={
-		"article":get_object_or_404(Article.objects.published() , slug=slug)
-	}
-	return render(request,"blog/detail.html",context)'''
-
-# class ArticleDetail(DetailView):
-# 	def get_object(self):
-# 		slug=self.kwargs.get('slug')
-# 		return get_object_or_404(Article.objects.published(), slug=slug)
 
 
 def show_article_detail(request , slug):
@@ -71,53 +50,10 @@ def show_article_detail(request , slug):
 
 	context={
 		"article":get_object_or_404(Article.objects.published() , slug=slug),
+		"cats":Category.objects.all(),
 	}
 	return render(request , "blog/blog-details.html" , context)
 
-
-
-
-# def show_article_detail(request , slug):
-# 	User=get_user_model()
-# 	users=User.objects.all()
-
-# 	cf = CommentForm(request.POST or None)
-# 	if request.method == 'POST':
-# 		if cf.is_valid():
-# 			comment = cf.save(commit=False)
-# 			comment.article=get_object_or_404(Article.objects.all() , slug=slug)
-# 			if request.user in users:
-# 				comment.user=request.user
-# 			comment.save()
-# 			return HttpResponseRedirect(f"http://127.0.0.1:8000/article/{slug}")
-# 		else:
-# 			cf = CommentForm()
-
-# 	context={
-# 		"article":get_object_or_404(Article.objects.published() , slug=slug),
-# 		'comment_form':cf,
-# 	}
-# 	return render(request , "blog/blog-details.html" , context)
-
-
-
-# def comment_save_view(request, id):
-# 	print("this is running&&&&&&&&&&$$$$")
-# 	if request.method == 'POST':
-# 		cf = CommentForm(request.POST or None)
-# 		if cf.is_valid():
-# 			content = request.POST.get('content')
-# 			comment = Comment.objects.create(post = Article, user = request.user, content = content)
-# 			comment.save()
-# 			return redirect(Article.get_absolute_url())
-# 		else:
-# 			cf = CommentForm()
-		
-# 		context ={
-# 		'comment_form':cf,
-# 		}
-# 		return render(request, 'blog/detail.html', context)
-	
 
 
 '''
@@ -135,7 +71,7 @@ def category(request, slug ,page=1):
 
 class CategoryList(ListView):
 	paginate_by = 3
-	template_name='blog/category_list.html'
+	template_name='blog/category-list.html'
 
 	def get_queryset(self):
 		global category
@@ -174,7 +110,7 @@ def show_base_page(request):
 	return render(request, "blog/base.html" )
 
 
-
+#api_views
 
 class ArticleViewSet(viewsets.ModelViewSet):
     """
