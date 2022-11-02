c4739987bfbddc9d3c463d015652f81f9ea90701
amin <biglarymohamadamin75@gmail.com>
2022-11-02 16:57:50 +0330

    get wallpaper-url in index.html with get_absolute_url

--- presentation_app/views.py
+++ presentation_app/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render,HttpResponse
 from .models import ImageModel
 
 def show_main_page(request):
-    # the_image = ImageModel.objects.get(name="wallpaper3")
-    the_image = ImageModel.objects.get(id=123)
-    return render(request,"presentation_blog/index.html", {"the_image":the_image})
+    first_wallpaper = ImageModel.objects.get(name="wallpaper1")
+    second_wallpaper = ImageModel.objects.get(name="wallpaper2")
+    return render(request,"presentation_blog/index.html", {"first_wallpaper":first_wallpaper,
+                                                            "second_wallpaper":second_wallpaper})
 
 
 def show_services(request):
