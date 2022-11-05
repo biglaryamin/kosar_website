66aa4cd270dc59446293aae75c26174e1e2301b6
amin <biglarymohamadamin75@gmail.com>
2022-11-05 13:01:21 +0330

    add __init__ for TextForm
    added for changing default label in TextForm

--- presentation_app/forms.py
+++ presentation_app/forms.py
@@ -12,3 +12,11 @@ class TextForm(ModelForm):
     class Meta:
         model = TextModel
         fields = '__all__'
+
+    
+    def __init__(self, *args, **kwargs):
+        super(TextForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "عنوان متن"
+        self.fields['pre_text'].label = "پیش متن"
+        self.fields['text'].label = "متن اصلی"
+        self.fields['post_text'].label = "پس از متن"
