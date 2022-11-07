8da793940aeb641894e817043f1a1ed42901db08
amin <biglarymohamadamin75@gmail.com>
2022-11-07 11:02:27 +0330

    add crawled article model

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CrawledArticle(models.Model):
+    title = models.CharField(max_length=50, null=True, blank=True, verbose_name="عنوان")
+    text = models.TextField(null=True, blank=True, verbose_name="متن")
+
+
+    def __str__(self):
+        return self.title
+
+    
+    class Meta:
+        verbose_name = "مقاله اینترنتی"
+        verbose_name_plural = "مقاله های اینترنتی"
