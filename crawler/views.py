5f9032631bbc8b6393414c273acd02a5c3dfe12d
amin <biglarymohamadamin75@gmail.com>
2022-11-09 13:59:16 +0330

    url-field in CrawledArticle handled in views(needs to refactor)

--- crawler/views.py
+++ crawler/views.py
@@ -14,38 +14,51 @@ def crawler(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     titles = soup.select("tr td span.titleline")
-    return titles
+
+    temp_links = soup.select("tr td span a")
+    links = []
+    for link in temp_links:
+        try:
+            temp_link = re.findall(r".*href=(.*?)\>", str(link))
+            links.append(temp_link)
+        except:
+            pass
+
+    return (titles, links)
 
 
-# def get_links(url):
-#     page = requests.get(url)
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     links = soup.select("tr td span a")
-#     result = re.findall(r".*href=(.*?)\>", link)
-#     for link in links:
-#         print(link)
-#         print("****************************")
-
-    
+def remove_extra_letter(url):
+    end = len(url)-1
+    result = url[2:end-1]
+    return result
 
 
 def test(request):
+    counter = 2
     url = "https://news.ycombinator.com/"
     # get_links(url)
+    links = crawler(url)[1]
+    print(f"--------------{links}------------------------")
     articles = CrawledArticle.objects.all().order_by('-created')
     local_articles = []
     for article in articles:
         local_articles.append(str(article.title))
         
-    for link in crawler(url):
+    for link in crawler(url)[0]:
+        correct_link = remove_extra_letter(str(links[counter]))
         try:
-            the_article = CrawledArticle(title=link.text)
-            if str(the_article.title) not in local_articles:
-                the_article.save()
+            the_article = CrawledArticle(title=link.text, link = correct_link)
+            print("works")
+        except IndexError:
+            the_article = CrawledArticle(title=link.text, link = "index error!")
+            print("error")
+        
+        if str(the_article.title) not in local_articles:
+            the_article.save()
 
-        except:
-            pass
 
-    author = ""
+        counter += 1
+    
+    articles = CrawledArticle.objects.all().order_by('-created')
     print(f"******* number of all articles are : {articles.count()} **********")
-    return render(request, "crawler/list.html", {"articles":articles, "author":author})
+    return render(request, "crawler/list.html", {"articles":articles})
